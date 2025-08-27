ab05a6ecc019416e1a35ff5c0b3c5d7d57296db5
Nam Nguyen <hoangnam9194@gmail.com>
2025-08-27 15:38:35 +0700

    update new endpoint

--- polargx/src/main/java/com/library/polargx/Configuration.kt
+++ polargx/src/main/java/com/library/polargx/Configuration.kt
@@ -8,13 +8,13 @@ open class EnvConfiguration(
 
 class DevEnvConfiguration(
     override val name: String = "Development",
-    override val server: String = "lydxigat68.execute-api.us-east-1.amazonaws.com/dev",
+    override val server: String = "8mr6rftgmb.execute-api.us-east-1.amazonaws.com/dev",
     override val supportedBaseDomains: String = ".makelabs.ai"
 ) : EnvConfiguration(name, server, supportedBaseDomains)
 
 class ProdEnvConfiguration(
     override val name: String = "Production",
-    override val server: String = "lydxigat68.execute-api.us-east-1.amazonaws.com/prod",
+    override val server: String = "8mr6rftgmb.execute-api.us-east-1.amazonaws.com/prod",
     override val supportedBaseDomains: String = ".gxlnk.com"
 ) : EnvConfiguration(name, server, supportedBaseDomains)
 
